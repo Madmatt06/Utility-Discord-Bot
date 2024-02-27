12ba5b9d870699e9b79dc6b0768c5a0b37cf3116
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2024-02-27 07:37:10 -0800

    Added UserID for bot owner to allow debugging or command tree refreshing

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+# Loads all the neccesary settings from .env
+
 load_dotenv()
 
 DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
+BOT_OWNER_ID = os.getenv("BOT_OWNER_ID")
