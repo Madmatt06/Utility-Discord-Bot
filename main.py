2fdbdc896d76d4b1b16f590fe6264a6907d8f981
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2024-02-27 07:38:29 -0800

    Created bot and commands to lock a person to a nickname, unlock, refresh command tree, and ping the bot. A function has also been created for when a nickname has been changed to change it back if the user has been locked.

---
+++ main.py
@@ -0,0 +1,83 @@
+import settings
+import discord
+from discord.ext import commands
+from discord import app_commands
+from nick_user import nick_user
+
+def run():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.members = True
+    bot = commands.Bot(command_prefix="#", intents = intents)
+    nick_users_checkin:list[discord.User] = []
+    PERM_ERROR = "Sorry! You dont have the proper permissions for that!"
+    @bot.event
+    async def on_ready():
+        print(f"Bot Name: {bot.user}")
+        print(f"Bot ID: {bot.user.id}")
+        print("--------------------------------------")
+
+    @bot.event
+    async def on_member_update(memberBefore:discord.User, memberAfter:discord.User):
+        for user_nick in nick_users_checkin:
+            if memberBefore.id == user_nick.user.id and memberAfter.nick != user_nick.nick:
+                await memberAfter.edit(nick=user_nick.nick)
+                return
+
+    @bot.tree.command(name="ping", description="allows you to test if the bot is functioning")
+    async def ping(interaction:discord.Interaction):
+        await interaction.response.send_message("pong", ephemeral=True)
+
+    @bot.tree.command(name="sync_tree", description="Syncs the commands with the guild. ONLY FOR ADMINS")
+    async def sync_tree(interaction:discord.Interaction):
+        if not interaction.user.guild_permissions.administrator or not str(interaction.user.id) == settings.BOT_OWNER_ID:
+            await interaction.response.send_message(PERM_ERROR, ephemeral = True)
+            return
+        await interaction.response.send_message("Syncing...", ephemeral = True)
+        await bot.tree.sync()
+        print('Command tree synced.')
+        message = await interaction.original_response()
+        await message.edit(content="Done")
+    
+    @bot.tree.command(name="force_nick", description="Allows you to force a user with permission to change nick to have a specific name")
+    @app_commands.choices()
+    @commands.has_permissions(manage_nicknames = True)
+    async def force_nick(interaction:discord.Interaction, username: discord.Member, nick:str):
+        if not interaction.user.guild_permissions.manage_nicknames:
+            interaction.response.send_message(PERM_ERROR, ephemeral=True)
+            return
+        if not interaction.user.guild_permissions.administrator:
+            interaction.response.send_message(PERM_ERROR, ephemeral= True)
+            return
+        await interaction.response.send_message("Editing name", ephemeral=True)
+        message = await interaction.original_response()
+        for user in nick_users_checkin:
+            if user.user.id == username.id:
+                await message.edit(content="There is already a nickname lock for this user")
+                return
+        nick_users_checkin.append(nick_user(user=username,nick=nick))
+        await username.edit(nick = nick)
+        await message.edit(content="Done")
+
+    @bot.tree.command(name="remove_nick_lock", description="Allows you remove a locked nickname")
+    @app_commands.choices()
+    @commands.has_permissions(manage_nicknames = True)
+    async def remove_lock_nick(interaction:discord.Interaction, username: discord.Member):
+        if(not interaction.user.guild_permissions.administrator):
+            await interaction.response.send_message(PERM_ERROR, ephemeral=True)
+            return
+        await interaction.response.send_message("Removing...", ephemeral= True)
+        message = await interaction.original_response()
+        i = 0
+        for user in nick_users_checkin:
+            if user.id == username.id:
+                nick_users_checkin.pop(i)
+                await message.edit(content="Done")
+                return
+            i+= 1
+        await message.edit(content="No Locks found for user")
+
+    bot.run(settings.DISCORD_API_SECRET)
+
+if __name__ == "__main__":
+    run()
