b2b5bae936eb3acac3a098671dc8799fd9dafd32
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2024-07-11 19:00:44 -0700

    update stuff i guess

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 from discord import app_commands
 from nick_user import nick_user
 import sys
-
+import requests
+from random import randint
 def run():
     intents = discord.Intents.default()
     intents.message_content = True
@@ -100,8 +101,24 @@ def run():
     @bot.tree.command(name="say", description="allows the bot to say things")
     @app_commands.choices()
     async def say(interaction:discord.Interaction, say: str):
+        if(not interaction.user.guild_permissions.administrator):
+            return
         await send_message(message=say, channel=interaction.channel)
         await respond_message(message="Done",interaction=interaction, ephemeral=True)
+    @bot.tree.command(name="cat", description="Generates a cat")
+    async def cat(interaction:discord.Interaction):
+        img_data = requests.get("https://genrandom.com/api/cat").content
+        with open('image_name.jpg', 'wb') as image:
+            image.write(img_data)
+        with open('image_name.jpg', 'rb') as image:
+            imageFile = discord.File(image)
+            await interaction.response.send_message(file=imageFile)
+
+    @bot.tree.command(name="cattp", description="Random HTTP code cat style")
+    async def cattp(interaction:discord.Interaction):
+        http_codes = [100,101,102,103,200,201,202,203,204,205,206,207,208,214,226,300,301,302,303,304,305,307,308,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,421,422,423,424,425,426,428,429,431,444,450,451,497,498,499,500,501,502,503,504,506,507,508,509,510,511,521,522,523,525,530,599]
+        http_code = http_codes[randint(0, len(http_codes)-1)]
+        await interaction.response.send_message("https://http.cat/" + str(http_code))
 
     @bot.tree.command(name="stop", description="Shutsdown the bot")
     async def stop(interaction:discord.Interaction):
