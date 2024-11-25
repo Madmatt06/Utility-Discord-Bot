30c510c649561655996f49cc8277ca827d99b4eb
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2024-11-24 20:14:39 -0800

    Reformatted to double space

--- src/generate_env.py
+++ src/generate_env.py
@@ -1,4 +1,4 @@
 import dotenv
 if __name__ == "__main__":
-    dotenv.set_key(".env", "DISCORD_API_TOKEN", "")
-    dotenv.set_key(".env", "BOT_OWNER_ID", "")
+  dotenv.set_key(".env", "DISCORD_API_TOKEN", "")
+  dotenv.set_key(".env", "BOT_OWNER_ID", "")
