fc02fcb3a34e73193f484e0d92bcc7e1868145ce
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2024-12-14 09:08:02 -0800

    cattp was moved to a cog

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from discord import app_commands
 import sys
 import requests
-from random import randint
 import time
 import os
 from cogs.defaults import *
@@ -91,11 +90,6 @@ def run():
       imageFile = discord.File(image)
       await interaction.response.send_message(file=imageFile)
 
-  @bot.tree.command(name="cattp", description="Random HTTP code cat style")
-  async def cattp(interaction:discord.Interaction):
-    http_codes = [100,101,102,103,200,201,202,203,204,205,206,207,208,214,226,300,301,302,303,304,305,307,308,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,421,422,423,424,425,426,428,429,431,444,450,451,497,498,499,500,501,502,503,504,506,507,508,509,510,511,521,522,523,525,530,599]
-    http_code = http_codes[randint(0, len(http_codes)-1)]
-    await interaction.response.send_message("https://http.cat/" + str(http_code))
 
 
 
