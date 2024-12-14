072353c5ec9949e12102f2b5ae9a7c6bf1a1e2ef
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2024-12-14 09:32:11 -0800

    Moved sync_tree to a cog

---
+++ src/cogs/sync_tree/COG_sync_tree.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+from discord import app_commands,Interaction
+from src.cogs.defaults import *
+from src.cogs.bot_library import respond_message,edit_message
+import src.settings as settings
+import time
+
+class sync_tree(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.last_sync:float = time.time()
+
+    @app_commands.command(name="sync_tree", description="Syncs the cogs with the guild. ONLY FOR ADMINS")
+    async def sync_tree(self, interaction: Interaction):
+        if not interaction.user.guild_permissions.administrator and str(interaction.user.id) != settings.BOT_OWNER_ID:
+            await respond_message(message=PERM_ERROR, interaction=interaction, ephemeral=True)
+            return
+
+        if (time.time() - self.last_sync) < 30:
+            await respond_message(
+                message="sync_tree unavailable. Please wait at least 30 seconds between sync_tree calls.",
+                interaction=interaction, ephemeral=True)
+            return
+
+        await respond_message(message="Syncing...", interaction=interaction, ephemeral=True)
+        await self.bot.tree.sync()
+        print("Command tree synced.")
+        message = await interaction.original_response()
+        await edit_message(edit="Done", message=message)
+        self.last_sync = time.time()
+
+
+async def setup(bot):
+    await bot.add_cog(sync_tree(bot))
+    print("sync_tree is loaded")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 import discord
 from discord.ext import commands
 import sys
-import time
 import os
-from cogs.defaults import *
 from cogs.bot_library import send_message,respond_message,edit_message
 import settings
-
-last_sync:float = time.time()
 
 
 
@@ -16,9 +12,6 @@ def run():
   intents.message_content = True
   intents.members = True
   bot = commands.Bot(command_prefix="#", intents = intents)
-
-
-
 
   async def load_cog(cog_path:str):
     print(f"Loading Cog {cog_path[2:-3].replace('/', '.').replace('\\', '.')}")
@@ -37,7 +30,6 @@ def run():
         if os.path.isdir(new_path):
           await load_cogs(new_path, LEVEL+1)
 
-
   @bot.event
   async def on_ready():
     print("Loading cogs")
@@ -46,26 +38,6 @@ def run():
     print(f"Bot Name: {bot.user}")
     print(f"Bot ID: {bot.user.id}")
     print("--------------------------------------")
-
-
-  @bot.tree.command(name="sync_tree", description="Syncs the cogs with the guild. ONLY FOR ADMINS")
-  async def sync_tree(interaction:discord.Interaction):
-    if not interaction.user.guild_permissions.administrator and str(interaction.user.id) != settings.BOT_OWNER_ID:
-      await respond_message(message=PERM_ERROR,interaction=interaction, ephemeral = True)
-      return
-
-    global last_sync
-    if (time.time() - last_sync) < 30:
-      await respond_message(message="sync_tree unavailable. Please wait at least 30 seconds between sync_tree cogs.", interaction=interaction, ephemeral=True)
-      return
-
-    await respond_message(message="Syncing...",interaction=interaction, ephemeral = True)
-    await bot.tree.sync()
-    print("Command tree synced.")
-    message = await interaction.original_response()
-    await edit_message(edit="Done", message=message)
-    last_sync = time.time()
-
 
   @bot.tree.command(name="stop", description="Shutdown the bot")
   async def stop(interaction:discord.Interaction):
