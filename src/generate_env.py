efc95ca7fa81c55e2ea1ca77f0a5016bb0d02739
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-01-05 16:57:10 -0800

    Preparing for customizable commands

--- src/generate_env.py
+++ src/generate_env.py
@@ -5,12 +5,15 @@ if __name__ == "__main__":
   BOT_OWNER_ID:str = ""
   PREFIX_MESSAGE:str = ""
   answer = input("Would you like to set the enviroment keys now? (y/n) ")
+  COMMAND_SEPERATOR = "_"
   if answer.lower() == "y" or answer.lower() == "yes":
     DISCORD_API_TOKEN = input("Please type your bot token: ")
     BOT_OWNER_ID = input("Please type the discord id for the bot owner: ")
     PREFIX_MESSAGE = input("Please type the prefix you would like for messages (leave blank for nothing): ")
+    COMMAND_SEPERATOR = input("Please enter the seperator you would like for your commands (sync:seperator:tree): ")
+
 
   dotenv.set_key(".env", "DISCORD_API_TOKEN", DISCORD_API_TOKEN)
   dotenv.set_key(".env", "BOT_OWNER_ID", BOT_OWNER_ID)
   dotenv.set_key(".env", "PREFIX_MESSAGE", PREFIX_MESSAGE)
-  
+  dotenv.set_key(".env", "COMMAND_SEPERATOR", COMMAND_SEPERATOR)
