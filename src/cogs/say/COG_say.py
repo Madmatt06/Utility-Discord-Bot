666b2c74401af814929be94f6dcbfbef918a0f5b
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-01-05 22:42:28 -0800

    Added Settings for say permissions
    This can probably be done in a different way that is better.

--- src/cogs/say/COG_say.py
+++ src/cogs/say/COG_say.py
@@ -2,20 +2,25 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from src.cogs.bot_library import send_message, respond_message, create_command
+from src.settings import SAY_PERMS, BOT_OWNER_ID
 
 
 class Say(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-    @app_commands.command(name=create_command("say"), description="allows the bot to say things")
-    @app_commands.choices()
-    async def say(self, interaction: discord.Interaction, say: str):
-        if not interaction.user.guild_permissions.administrator:
-            return
-        await send_message(message=say, channel=interaction.channel)
-        await respond_message(message="Done", interaction=interaction, ephemeral=True)
+    if SAY_PERMS != "4":
+        @app_commands.command(name=create_command("say"), description="allows the bot to say things")
+        @app_commands.choices()
+        async def say(self, interaction: discord.Interaction, say: str):
+            if not interaction.user.id == BOT_OWNER_ID and SAY_PERMS == "0":
+                return
+            if not interaction.user.guild_permissions.administrator and SAY_PERMS == "1":
+                return
+            if not interaction.user.id == BOT_OWNER_ID and not interaction.user.guild_permissions.administrator and SAY_PERMS == "2":
+                return
+            await send_message(message=say, channel=interaction.channel)
+            await respond_message(message="Done", interaction=interaction, ephemeral=True)
 
 
 async def setup(bot):
