fbb56f555438580c85b6d7fab40a7f771408d678
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-01-09 22:05:46 -0800

    Added support for view in messages (in bot library)

--- src/cogs/bot_library.py
+++ src/cogs/bot_library.py
@@ -2,25 +2,25 @@ import discord
 import src.settings as settings
 
 
-async def respond_message(message: str, interaction: discord.Interaction, ephemeral: bool):
+async def respond_message(message: str, interaction: discord.Interaction, ephemeral: bool, view:discord.ui.View=None):
     if settings.PREFIX:
-        await interaction.response.send_message(f"{settings.PREFIX} ({message})", ephemeral=ephemeral)
+        await interaction.response.send_message(content=f"{settings.PREFIX} ({message})", ephemeral=ephemeral, view=view)
     else:
-        await interaction.response.send_message(message, ephemeral=ephemeral)
+        await interaction.response.send_message(content=message, ephemeral=ephemeral, view=view)
 
 
-async def edit_message(edit: str, message: discord.Interaction.original_response):
+async def edit_message(edit: str, message: discord.Interaction.original_response, view:discord.ui.View=None):
     if settings.PREFIX:
-        await message.edit(content=f"{settings.PREFIX} ({edit})")
+        await message.edit(content=f"{settings.PREFIX} ({edit})", view=view)
     else:
-        await message.edit(content=edit)
+        await message.edit(content=edit, view=view)
 
 
-async def send_message(message: str, channel: discord.Interaction.channel):
+async def send_message(message: str, channel: discord.Interaction.channel, view:discord.ui.View=None):
     if settings.PREFIX:
-        await channel.send(f"{settings.PREFIX} ({message})")
+        await channel.send(content=f"{settings.PREFIX} ({message})", view=view)
     else:
-        await channel.send(message)
+        await channel.send(content=message, view=view)
 
 def create_command(command: str) -> str:
     return command.replace(" ", settings.COMMAND_SEPERATOR)
