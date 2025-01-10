e35ddce4a356b0917a4a7b75a5c6608b083f3337
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-01-09 22:26:53 -0800

    bot_library no longer passes in None to discord.py
    Turns out discord.py doesn't like that and throws an error.

--- src/cogs/bot_library.py
+++ src/cogs/bot_library.py
@@ -4,23 +4,43 @@ import src.settings as settings
 
 async def respond_message(message: str, interaction: discord.Interaction, ephemeral: bool, view:discord.ui.View=None):
     if settings.PREFIX:
-        await interaction.response.send_message(content=f"{settings.PREFIX} ({message})", ephemeral=ephemeral, view=view)
+        if view is None:
+            await interaction.response.send_message(content=f"{settings.PREFIX} ({message})", ephemeral=ephemeral)
+        else:
+            await interaction.response.send_message(content=f"{settings.PREFIX} ({message})", ephemeral=ephemeral,
+                                                    view=view)
+
     else:
-        await interaction.response.send_message(content=message, ephemeral=ephemeral, view=view)
+        if view is None:
+            await interaction.response.send_message(content=message, ephemeral=ephemeral)
+        else:
+            await interaction.response.send_message(content=message, ephemeral=ephemeral, view=view)
 
 
 async def edit_message(edit: str, message: discord.Interaction.original_response, view:discord.ui.View=None):
     if settings.PREFIX:
-        await message.edit(content=f"{settings.PREFIX} ({edit})", view=view)
+        if view is None:
+            await message.edit(content=f"{settings.PREFIX} ({edit})")
+        else:
+            await message.edit(content=f"{settings.PREFIX} ({edit})", view=view)
     else:
-        await message.edit(content=edit, view=view)
+        if view is None:
+            await message.edit(content=edit)
+        else:
+            await message.edit(content=edit, view=view)
 
 
 async def send_message(message: str, channel: discord.Interaction.channel, view:discord.ui.View=None):
     if settings.PREFIX:
-        await channel.send(content=f"{settings.PREFIX} ({message})", view=view)
+        if view is None:
+            await channel.send(content=f"{settings.PREFIX} ({message})")
+        else:
+            await channel.send(content=f"{settings.PREFIX} ({message})", view=view)
     else:
-        await channel.send(content=message, view=view)
+        if view is None:
+            await channel.send(content=message)
+        else:
+            await channel.send(content=message, view=view)
 
 def create_command(command: str) -> str:
     return command.replace(" ", settings.COMMAND_SEPERATOR)
