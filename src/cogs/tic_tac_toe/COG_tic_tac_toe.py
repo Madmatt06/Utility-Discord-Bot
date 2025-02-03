c6d6c56a4d1986e7fea7feceea5e5b620ac80f73
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-02-02 18:33:24 -0800

    Added more commands for tic tac toe games

--- src/cogs/tic_tac_toe/COG_tic_tac_toe.py
+++ src/cogs/tic_tac_toe/COG_tic_tac_toe.py
@@ -9,9 +9,17 @@ class TicTacToe(commands.Cog):
         self.bot = bot
 
     tic_tac_toe = app_commands.Group(name=create_command("tic tac toe"), description="All commands for tic tac toe games")
-    @tic_tac_toe.command(name=create_command("start game"), description="allows you to start a game against someone")
-    async def ping(self, interaction: Interaction, opponent: discord.Member):
+    @tic_tac_toe.command(name=create_command("start"), description="Allows you to start a game against someone")
+    async def start(self, interaction: Interaction, opponent: discord.Member):
         await respond_message(message=f"Starting game against opponent {opponent.name}", interaction=interaction, ephemeral=True)
+
+    @tic_tac_toe.command(name=create_command("resume"), description="Allows you to resume a game if the last game interaction has expired")
+    async def resume(self, interaction: Interaction):
+        await respond_message(message=f"Resuming game", interaction=interaction, ephemeral=True)
+
+    @tic_tac_toe.command(name=create_command("get games"), description="Allows you to see what games you are currently included in and who you are against")
+    async def get_games(self, interaction: Interaction):
+        await respond_message(message=f"Getting game info", interaction=interaction, ephemeral=True)
 
 
 async def setup(bot):
