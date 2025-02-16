eed0f7bc0a28d5b26eae47c5a0db47bbe401743a
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-02-16 12:53:15 -0800

    Changed Cat command to save cat image to memory instead of writing a file.

--- src/cogs/cat/COG_cat.py
+++ src/cogs/cat/COG_cat.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from discord import app_commands
 import requests
 import discord
+from io import BytesIO
 
 from src.cogs.bot_library import create_command
 
@@ -10,15 +11,17 @@ class Cat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # TODO: Make faster by caching next image.
+    # TODO: Make faster by caching next image. Might actually not be needed since saving cat image in memory seems to be faster
     @app_commands.command(name=create_command("cat"), description="Generates a cat")
     async def cat(self, interaction: discord.Interaction):
+        # Gets a cat image and saves it to a BytesIO object. Then sends it.
         img_data = requests.get("https://genrandom.com/api/cat").content
-        with open('image_name.jpg', 'wb') as image:
-            image.write(img_data)
-        with open('image_name.jpg', 'rb') as image:
-            imageFile = discord.File(image)
-            await interaction.response.send_message(file=imageFile)
+        image_simul_file = BytesIO(img_data)
+        image_file = discord.File(image_simul_file)
+        image_file.filename = "cat.png"
+        image_file.description = "A random cat picture!"
+        await interaction.response.send_message(file=image_file)
+        image_simul_file.close()
 
 
 async def setup(bot):
