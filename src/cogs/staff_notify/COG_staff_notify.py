92822ef79248769613da8ca4975c55ca8ea3fc6a
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-04-06 12:15:05 -0700

    Added save system for staff_notify to allow the bot to be restarted and maintain the notification channels.

--- src/cogs/staff_notify/COG_staff_notify.py
+++ src/cogs/staff_notify/COG_staff_notify.py
@@ -1,16 +1,33 @@
-from discord.app_commands import guilds
+import os.path
 from discord.ext import commands
 from discord import app_commands
 import discord
-
 from src.cogs.bot_library import create_command, respond_message, send_message
-from src.cogs.defaults import PERM_ERROR
+from src.cogs.defaults import PERM_ERROR, SAVE_PATH
 
+
+SAVE = 'staff_notify'
 
 class Staff(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
-    self.guilds_saved: dict[int, int] = {}
+
+    # Load Save Data
+    data: dict[int, int] = {}
+    if os.path.isfile(f'{SAVE_PATH}/{SAVE}'):
+      with open(f'{SAVE_PATH}/{SAVE}', 'r') as save_file:
+        save_data: str = save_file.read()
+        save_data_by_guild: [str] = save_data.splitlines()
+        for guild in save_data_by_guild:
+          guild_data: [str] = guild.split(' ')
+          try:
+            data[int(guild_data[0])] = int(guild_data[1])
+          except ValueError:
+            print('Invalid save data found. Skipping...')
+          except Exception as error:
+            print(f'unknown error occurred. Stopping Load. {error}')
+            return
+    self.guilds_saved:[int, int] = data
 
   @app_commands.command(name=create_command('notify channel'), description='Sets channel for notifications to appear')
   async def set_staff_channel(self, interaction: discord.Interaction):
@@ -28,6 +45,15 @@ class Staff(commands.Cog):
     await send_message(message=message, channel=notify_channel, view=view)
     return True
 
+  async def cog_unload(self) -> None:
+    try:
+       with open(f'{SAVE_PATH}/{SAVE}', 'w') as save_file:
+         for guild in self.guilds_saved:
+           save_file.write(f'{guild} {self.guilds_saved[guild]}\n')
+    except PermissionError:
+      print('Unable to write save file due to permission errors')
+      return
+    print('Saved staff_notify settings')
 
 async def setup(bot):
   await bot.add_cog(Staff(bot))
