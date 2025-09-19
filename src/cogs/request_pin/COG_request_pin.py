c45a898f1ab92c55f4529db85e329650702feba6
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-09-18 23:49:13 -0700

    Removed sketchy code that gets message to pin and replaced it with data stored in class for buttons. Also adjusted both reason to pin and pin request message to include who is requesting the pin and who accepts the pin.

--- src/cogs/request_pin/COG_request_pin.py
+++ src/cogs/request_pin/COG_request_pin.py
@@ -6,40 +6,29 @@ from src.cogs.bot_library import create_command, respond_message, edit_message, 
 from src.cogs.staff_notify import COG_staff_notify
 
 class Buttons(discord.ui.View):
-  def __init__(self, *, timeout=None, bot:discord.ClientUser.bot):
+  def __init__(self, *, timeout=None, bot:discord.ClientUser.bot, message_id:int, channel_id:int, requester_id:int):
     super().__init__(timeout=timeout)
     self.bot = bot
+    self.message_id = message_id
+    self.channel_id = channel_id
+    self.requester_id = requester_id
   @discord.ui.button(label='Pin Message',style=discord.ButtonStyle.green)
   async def accept_button(self,interaction:discord.Interaction, button:discord.ui.Button):
-    #Todo: Look in to passing in a message object or ids directly in the init function instead of this sketchy decoding
-    target_message: str = interaction.message.content
-    print(f'Decoding bot message for ids \"{target_message}\"')
-    starter = 'https://discord.com/channels/'
-    starting_id: int = target_message.find(starter)
-    repeat_message_link: str = target_message[starting_id:]
-    if repeat_message_link.endswith(')'):
-      repeat_message_link = repeat_message_link[:-1]
-    target_message = target_message[starting_id + len(starter):]
-    next_seperator: int = target_message.find('/')
-    guild_id: str = target_message[:next_seperator]
-    target_message = target_message[next_seperator + 1:]
-    next_seperator = target_message.find('/')
-    channel_id: str = target_message[:next_seperator]
-    target_message = target_message[next_seperator + 1:]
-    ending: int = 0
-    for letter in target_message:
-      if letter.isdigit():
-        ending += 1
-      else:
-        break
-    message_id: str = target_message[:ending]
-    print(f'Getting message with guild "{guild_id}", Channel "{channel_id}", and message id "{message_id}"')
-    channel: discord.TextChannel = self.bot.get_channel(int(channel_id))
-    message_to_pin: discord.Message = await channel.fetch_message(int(message_id))
+    channel: discord.TextChannel = self.bot.get_channel(self.channel_id)
+    message_to_pin: discord.Message = await channel.fetch_message(self.message_id)
+    requester = channel.guild.get_member(self.requester_id)
+    requester_name = requester.name
+    requester_nick = requester.display_name
+    accepter_name = interaction.user.name
+    accepter_nick = interaction.user.display_name
     try:
-      await message_to_pin.pin(reason=create_text("Pin request accepted"))
+      await message_to_pin.pin(reason=create_text(f'Pin request from {requester_name} '
+                                                  f'{f'(goes by: {requester_nick})' if requester_nick is not None else ''}'
+                                                  f' accepted by '
+                                                  f'{accepter_name} '
+                                                  f'{f'(goes by: {requester_nick})' if accepter_nick is not None else ''}'))
     except discord.errors.HTTPException as error:
-      print(f'Recieved error when attempting to pin {error.code} with explanation {await error.response.text()}')
+      print(f'Received error when attempting to pin {error.code} with explanation {await error.response.text()}')
       if error.code == 50021:
         button.disabled = True
         await edit_message(message='I could not pin this message', original_response=interaction.message, view=self)
@@ -49,25 +38,16 @@ class Buttons(discord.ui.View):
       await interaction.response.defer()
       return
     self.clear_items()
-    await edit_message(message=f'Message Pinned! {repeat_message_link}', original_response=interaction.message,
-                       view=self)
+    await edit_message(message=f'Message Pinned! https://discord.com/channels/{message_to_pin.guild.id}/'
+                               f'{self.channel_id}/{self.message_id}', original_response=interaction.message, view=self)
     await interaction.response.defer()
   @discord.ui.button(label='Decline Request',style=discord.ButtonStyle.danger)
   async def decline_button(self, interaction:discord.Interaction, button:discord.ui.Button):
-    repeat_message_link:str = ''
-    target_message: str = interaction.message.content
-    print(f'Extracting discord message link \"{target_message}\"')
-    starter = 'https://discord.com/channels/'
-    starting_id: int = target_message.find(starter)
-    if starting_id != -1:
-      repeat_message_link = target_message[starting_id:]
-      if repeat_message_link.endswith(')'):
-        repeat_message_link = repeat_message_link[:-1]
-    else:
-      print(f'No Message link was left in request. Repeat is {repeat_message_link}')
     self.clear_items()
-    await edit_message(message=f'Pin request declined {repeat_message_link}', original_response=interaction.message,
-                       view=self)
+    channel: discord.TextChannel = self.bot.get_channel(self.channel_id)
+    message_to_pin: discord.Message = await channel.fetch_message(self.message_id)
+    await edit_message(message=f'Pin request declined for https://discord.com/channels/{message_to_pin.guild.id}/'
+                               f'{channel.id}/{message_to_pin.id}', original_response=interaction.message, view=self)
     await interaction.response.defer()
 
 
@@ -89,10 +69,13 @@ class RequestPin(commands.Cog):
       await respond_message(message='Something has gone wrong!', interaction=interaction, ephemeral=True)
       print('request_pin has failed to get staff_notify cog!')
       return
-    sent:bool = await staff_cog.notify_staff(message=f'Someone has requested a message to be pinned. '
+    sent:bool = await staff_cog.notify_staff(message=f'<@{interaction.user.id}> has requested a message to be pinned. '
                                                      f'https://discord.com/channels/{message.guild.id}'
                                                      f'/{message.channel.id}/{message.id}',
-                                             guild_id=interaction.guild_id, view=Buttons(bot=self.bot))
+                                             guild_id=interaction.guild_id,
+                                             view=Buttons(bot=self.bot,message_id=message.id,
+                                                          channel_id=message.channel.id,
+                                                          requester_id=interaction.user.id))
     if not sent:
       await respond_message(
         message='The owner hasn\'t set up some features yet. Ask the server admins to setup a staff notification chat '
